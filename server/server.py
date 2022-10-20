40bc162601ce33643d949d8577f3b06942f5f7c6
satancucumber <96267025+satancucumber@users.noreply.github.com>
2022-10-20 19:45:35 +0300

    Start of development
    Project construct

---
+++ server/server.py
@@ -0,0 +1,13 @@
+import index_main   # main page
+import index_rec    # recommendation page
+import index_fridge     # fridge page
+import index_recipes    # favorite recipes page
+import index_cuisines   # favorite cuisines page
+import index_account    # account page
+import index_inf    # information page
+
+import redis
+#import psycopg2-binary | psycopg2 (libpq, libssl)
+import html
+import socket
+
